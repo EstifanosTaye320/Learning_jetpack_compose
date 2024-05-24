cd5cd10e13e92a4172ca5583580ea0ba46cf6b92
EstifanosTaye320 <estifanostaye11@gmail.com>
2024-05-24 18:07:01 +0300

    made a snackbar

--- practice_area/app/src/main/java/com/example/practice_area/MainActivity.kt
+++ practice_area/app/src/main/java/com/example/practice_area/MainActivity.kt
@@ -1,131 +1,79 @@
 package com.example.practice_area
 
-import android.app.Fragment
-import android.content.res.Configuration
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarData
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.practice_area.ui.theme.Practice_areaTheme
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-        }
-    }
-}
-
-data class Event(var title: String = "Calender", var guestCount: Int = 0, var owner: String = "Estifanos Taye", var callLink: String = "https//:localhost/link", var guests: String = "guest list...") {
-    fun isGreaterThan50 (): Boolean {
-        return guestCount > 50
-    }
-
-    fun name () {}
-}
-
-@Composable
-fun SetTitle(title: String) {
-    Surface (
-        modifier = Modifier
-            .fillMaxWidth()
-            .border(
-                border = BorderStroke(2.dp, Color.Black),
-                shape = RoundedCornerShape(5.dp)
-            )
-    ) {
-                Text(text = "Title: $title", modifier = Modifier.padding(5.dp))
+            MySnackbarDemo()        }
     }
 }
 
 @Composable
-fun SetOwner(owner: String) {
-    Row (modifier = Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically) {
-            Surface (modifier = Modifier.size(width = 30.dp, height = 30.dp), shape = CircleShape,border = BorderStroke(width = 1.dp, color = Color.Black), color = Color.Green) {}
-            Text(text = "Owner: $owner", modifier = Modifier.padding(5.dp), fontFamily = FontFamily.Cursive, fontSize = 30.sp)
-    }
+fun MySnackbarDemo() {
+    var showSnackBar11 by remember { mutableIntStateOf(0) }
 
+    val snackbarHostState  = remember { SnackbarHostState() }
 
-}
+    val cupertin = rememberCoroutineScope()
 
-@Composable
-fun SetCallLink(link: String, room: String = "") {
-    Surface (
-        modifier = Modifier
-            .fillMaxWidth()
-            .border(
-                border = BorderStroke(2.dp, Color.Black),
-                shape = RoundedCornerShape(5.dp)
-            )
-            ) {
-        Column (modifier = Modifier.padding(5.dp)){
-            Text(text = "Link: $link")
-            Text(text = if (room != "") "Room: $room" else "")
-        }
-    }
-}
+    Scaffold (modifier = Modifier.fillMaxSize(), snackbarHost = {
+        SnackbarHost (hostState = snackbarHostState)
+    }) {
 
-@Composable
-fun CalenderCard(event: Event) {
-    Surface (
-        modifier = Modifier.fillMaxSize(),
-        border = BorderStroke(width = 2.dp, color = Color.Black),
-        shape = RoundedCornerShape(2.dp)
+        Column (modifier = Modifier
+            .padding(it)
+            .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
 
-    ) {
-        Column(verticalArrangement = Arrangement.Center) {
-            SetTitle(title = event.title)
-
-            Spacer(modifier = Modifier.height(4.dp))
-
-            Surface (
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .border(
-                        border = BorderStroke(2.dp, Color.Black),
-                        shape = RoundedCornerShape(5.dp)
-                    )
-            ) {
-
+                    Button(onClick = {
+                        showSnackBar11++
+                        cupertin.launch {
+                            snackbarHostState.showSnackbar("Estif did it he created snackbar Number $showSnackBar11")
+                        }
+                    }) {
+                            Text("Show Snackbar")
             }
         }
     }
 }
 
-
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun PreviewCard () {
-    CalenderCard(event = Event())
+fun MySnackbarDemoPreview() {
+    MySnackbarDemo()
 }
